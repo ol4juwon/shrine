75540d8c4845455ff1ac5a72c5167cb3193b0143
ol4 juwon <olajuwonlawal@icloud.com>
2020-12-08 01:21:57 +0100

    app bar done

--- app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductGridFragment.java
+++ app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductGridFragment.java
@@ -2,9 +2,16 @@ package com.google.codelabs.mdc.java.shrine;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 public class ProductGridFragment extends Fragment {
@@ -13,6 +20,29 @@ public class ProductGridFragment extends Fragment {
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.shr_product_grid_fragment, container, false);
+        View view = inflater.inflate(R.layout.shr_product_grid_fragment, container, false);
+
+        setUpToolbar(view);
+        return view;
+    }
+    private  void setUpToolbar(View view){
+        Toolbar toolbar = view.findViewById(R.id.app_bar);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        if(activity != null){
+            activity.setSupportActionBar(toolbar);
+        }
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.shr_toolbar_menu,menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 }

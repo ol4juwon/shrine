5e609adc26d034f18bf457e30f19c42e029c2aca
ol4 juwon <olajuwonlawal@icloud.com>
2020-12-08 03:13:10 +0100

    added sttyling and font edits

--- app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductCardViewHolder.java
+++ app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductCardViewHolder.java
@@ -1,13 +1,27 @@
 package com.google.codelabs.mdc.java.shrine;
 
 import android.view.View;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.toolbox.NetworkImageView;
+
+import org.w3c.dom.Text;
+
 public class ProductCardViewHolder extends RecyclerView.ViewHolder {
+    public NetworkImageView productImage;
+    public TextView productTitle;
+    public TextView productPrice;
 
     public ProductCardViewHolder(@NonNull View itemView) {
         super(itemView);
         //TODO: Find and store views from itemView
+
+        productImage = itemView.findViewById(R.id.product_image);
+        productPrice = itemView.findViewById(R.id.product_price);
+        productTitle = itemView.findViewById(R.id.product_title);
+
     }
 }
